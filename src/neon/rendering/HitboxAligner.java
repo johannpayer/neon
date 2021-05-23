04ee12d78997de06c1590bc2f62785fcfd557240
Johann <25992597+flamesdev@users.noreply.github.com>
2021-05-23 17:18:28 -0400

    Added more hitbox alignment methods
    Added the "alignTopLeft", "alignTopRight", "alignBottomLeft", and "alignBottomRight" methods to the HitboxAligner class.

--- src/neon/rendering/HitboxAligner.java
+++ src/neon/rendering/HitboxAligner.java
@@ -5,6 +5,24 @@ import neon.physics.Hitbox;
 /** A class used to align hitboxes. */
 public class HitboxAligner {
   /**
+   * Aligns the hitbox to its top.
+   *
+   * @param hitbox the hitbox
+   */
+  public static void alignTop(Hitbox hitbox) {
+    hitbox.setHigherYBound(hitbox.getCenter().getY());
+  }
+
+  /**
+   * Aligns the hitbox to its bottom.
+   *
+   * @param hitbox the hitbox
+   */
+  public static void alignBottom(Hitbox hitbox) {
+    hitbox.setLowerYBound(hitbox.getCenter().getY());
+  }
+
+  /**
    * Aligns the hitbox to its left.
    *
    * @param hitbox the hitbox
@@ -23,20 +41,42 @@ public class HitboxAligner {
   }
 
   /**
-   * Aligns the hitbox to its bottom.
+   * Aligns the hitbox to its top left.
    *
    * @param hitbox the hitbox
    */
-  public static void alignBottom(Hitbox hitbox) {
-    hitbox.setLowerYBound(hitbox.getCenter().getY());
+  public static void alignTopLeft(Hitbox hitbox) {
+    alignTop(hitbox);
+    alignLeft(hitbox);
   }
 
   /**
-   * Aligns the hitbox to its top.
+   * Aligns the hitbox to its top right.
    *
    * @param hitbox the hitbox
    */
-  public static void alignTop(Hitbox hitbox) {
-    hitbox.setHigherYBound(hitbox.getCenter().getY());
+  public static void alignTopRight(Hitbox hitbox) {
+    alignTop(hitbox);
+    alignRight(hitbox);
+  }
+
+  /**
+   * Aligns the hitbox to its bottom left.
+   *
+   * @param hitbox the hitbox
+   */
+  public static void alignBottomLeft(Hitbox hitbox) {
+    alignBottom(hitbox);
+    alignLeft(hitbox);
+  }
+
+  /**
+   * Aligns the hitbox to its bottom right.
+   *
+   * @param hitbox the hitbox
+   */
+  public static void alignBottomRight(Hitbox hitbox) {
+    alignBottom(hitbox);
+    alignRight(hitbox);
   }
 }
