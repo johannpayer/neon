801995a783ef4490e558846b8aeaa15886fd413d
Johann <25992597+flamesdev@users.noreply.github.com>
2021-05-30 09:45:31 -0400

    Replaced align hitbox methods with a single method
    - Added the "align" method to the HitboxAligner class.
    - Added the HitboxAlignment enum.
    - Removed the "alignTop", "alignBottom", "alignLeft", "alignRight", "alignTopLeft", "alignTopRight", "alignBottomLeft", and "alignBottomRight" methods in the HitboxAligner class.

--- src/neon/rendering/HitboxAligner.java
+++ src/neon/rendering/HitboxAligner.java
@@ -4,79 +4,50 @@ import neon.physics.Hitbox;
 
 /** A class used to align hitboxes. */
 public class HitboxAligner {
-  /**
-   * Aligns the hitbox to its top.
-   *
-   * @param hitbox the hitbox
-   */
-  public static void alignTop(Hitbox hitbox) {
+  private static void alignTop(Hitbox hitbox) {
     hitbox.setHigherYBound(hitbox.getCenter().getY());
   }
 
-  /**
-   * Aligns the hitbox to its bottom.
-   *
-   * @param hitbox the hitbox
-   */
-  public static void alignBottom(Hitbox hitbox) {
+  private static void alignBottom(Hitbox hitbox) {
     hitbox.setLowerYBound(hitbox.getCenter().getY());
   }
 
-  /**
-   * Aligns the hitbox to its left.
-   *
-   * @param hitbox the hitbox
-   */
-  public static void alignLeft(Hitbox hitbox) {
+  private static void alignLeft(Hitbox hitbox) {
     hitbox.setLowerXBound(hitbox.getCenter().getX());
   }
 
-  /**
-   * Aligns the hitbox to its right.
-   *
-   * @param hitbox the hitbox
-   */
-  public static void alignRight(Hitbox hitbox) {
+  private static void alignRight(Hitbox hitbox) {
     hitbox.setHigherXBound(hitbox.getCenter().getX());
   }
 
   /**
-   * Aligns the hitbox to its top left.
+   * Aligns the hitbox to the specified alignment.
    *
    * @param hitbox the hitbox
+   * @param alignment the alignment
    */
-  public static void alignTopLeft(Hitbox hitbox) {
-    alignTop(hitbox);
-    alignLeft(hitbox);
-  }
-
-  /**
-   * Aligns the hitbox to its top right.
-   *
-   * @param hitbox the hitbox
-   */
-  public static void alignTopRight(Hitbox hitbox) {
-    alignTop(hitbox);
-    alignRight(hitbox);
-  }
-
-  /**
-   * Aligns the hitbox to its bottom left.
-   *
-   * @param hitbox the hitbox
-   */
-  public static void alignBottomLeft(Hitbox hitbox) {
-    alignBottom(hitbox);
-    alignLeft(hitbox);
-  }
-
-  /**
-   * Aligns the hitbox to its bottom right.
-   *
-   * @param hitbox the hitbox
-   */
-  public static void alignBottomRight(Hitbox hitbox) {
-    alignBottom(hitbox);
-    alignRight(hitbox);
+  public static void align(Hitbox hitbox, HitboxAlignment alignment) {
+    switch (alignment) {
+      case TOP -> alignTop(hitbox);
+      case BOTTOM -> alignBottom(hitbox);
+      case LEFT -> alignLeft(hitbox);
+      case RIGHT -> alignRight(hitbox);
+      case TOP_LEFT -> {
+        alignTop(hitbox);
+        alignLeft(hitbox);
+      }
+      case TOP_RIGHT -> {
+        alignTop(hitbox);
+        alignRight(hitbox);
+      }
+      case BOTTOM_LEFT -> {
+        alignBottom(hitbox);
+        alignLeft(hitbox);
+      }
+      case BOTTOM_RIGHT -> {
+        alignBottom(hitbox);
+        alignRight(hitbox);
+      }
+    }
   }
 }
---
+++ src/neon/rendering/HitboxAlignment.java
@@ -0,0 +1,12 @@
+package neon.rendering;
+
+public enum HitboxAlignment {
+  TOP,
+  BOTTOM,
+  LEFT,
+  RIGHT,
+  TOP_LEFT,
+  TOP_RIGHT,
+  BOTTOM_LEFT,
+  BOTTOM_RIGHT
+}
